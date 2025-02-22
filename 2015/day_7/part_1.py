c0fcba5d56b03be530e9b0afc54dd78335e41863
Potato <kendrickyanuar@gmail.com>
2025-02-22 18:42:40 +0800

    Done 2015, day 7, part 1
    This took me way longer than it probably should have. I think my main issue is that I take too long planning my execution rather than trying it, because having to rewrite the code over and over again seemed tedious. However, I have learnt that rewriting code is not so bad if I properly document it, so that both current and future me can fully comprehend whatever I am writing.

--- 2015/day_7/part_1.py
+++ 2015/day_7/part_1.py
@@ -59,17 +59,108 @@ def provide_signals(instruction_lines: list[str]) -> dict:
 
             # Remove the instruction
             instruction_lines.remove(line)
-            
-    logging.debug(f'New instruction_lines:\n{instruction_lines}')
 
     return wire_dct, instruction_lines
 
+def connect_wires(wire_dct: dict, instruction_line: str) -> dict:
+    '''Connect the wires according to the given instruction and add them to the wire_dct.
+    
+    Returns the updated wire_dct with the new assigned wires.'''
+
+    words_list = instruction_line.split(' ')
+    wire_keys = wire_dct.keys()
+
+    logging.debug(f'Connecting {instruction_line}')
+    if len(words_list) == 5:
+        
+        output_wire = words_list[4]
+        if words_list[0] in wire_keys:
+            wire_1 = wire_dct[words_list[0]]
+        else:
+            wire_1 = int(words_list[0])
+            
+        if words_list[2] in wire_keys:
+            wire_2 = wire_dct[words_list[2]]
+        else:
+            wire_2 = int(words_list[2])
+        
+        match words_list[1]:
+            case 'AND':
+                value = wire_1 & wire_2
+            case 'OR':
+                value = wire_1 | wire_2
+            case 'RSHIFT':
+                value = wire_1 >> wire_2
+            case 'LSHIFT':
+                value = wire_1 << wire_2        
+        
+    elif len(words_list) == 4:
+        if words_list[0] == 'NOT':
+            output_wire = words_list[3]
+            value = ~ wire_dct[words_list[1]]
+
+    elif len(words_list) == 3:
+        output_wire = words_list[2]
+        value = wire_dct[words_list[0]]
+    
+    # Connect wire and update the wire_dct
+    wire_dct[output_wire] = value
+    # logging.debug(f'UPDATED wire_dct: {wire_dct}')
+
+    return wire_dct
+
+def find_lines(wire_dct: dict, instruction_lines: list[str]) -> dict:
+    '''Find all of the possible wires given the wire_dct.
+    
+    Returns the most updated wire_dct'''
+
+    ignored_lines = []
+    for line in instruction_lines:
+        do_connection = False
+        wire_keys = wire_dct.keys()
+        words_list = line.split(' ')
+
+        # If any of the input wires are not in wire_keys, ignore the line.
+        if len(words_list) == 5:
+
+            if not all(word.isdigit() or word in wire_keys for word in (words_list[0], words_list[2])):
+                ignored_lines.append(line)
+                # logging.debug(f'1) Ignoring line: {line}')
+                continue
+            
+        elif len(words_list) == 4:
+            # So lines like 'NOT cn -> co'
+
+            if words_list[1] not in wire_keys:
+                ignored_lines.append(line)
+                # logging.debug(f'2) Ignoring line: {line}')
+                continue
+
+        elif len(words_list) == 3:
+            # So lines like 'lx -> a'
+            if words_list[0] not in wire_keys:
+                ignored_lines.append(line)
+                continue
+        
+        else:
+            print(f'WHAT IS THIS: {line}')
+            continue
+
+        wire_dct = connect_wires(wire_dct, line)
+
+    # Keep recursively calling the function until no lines are ignored.
+    if len(ignored_lines) != 0:
+        logging.debug(f'Some lines are still ignored. Going through instructions with updated wire_dct now.')
+        wire_dct = find_lines(wire_dct, ignored_lines)
+
+    return wire_dct
+
 def main() -> None:
-    write_logs('part_1.log')
+    # write_logs('part_1.log')
     instruction_lines = get_input().splitlines()
-    logging.debug(f'New instruction_lines:\n{instruction_lines}')
     wire_dct, instruction_lines = provide_signals(instruction_lines)
-    logging.debug(wire_dct)
+    wire_dct = find_lines(wire_dct, instruction_lines)
+    print(f'Value of a: {wire_dct['a']}')
 
 if __name__ == '__main__':
     main()
