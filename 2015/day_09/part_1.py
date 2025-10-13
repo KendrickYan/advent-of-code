aec5720cfc7418fd54857579141912fe5bd6b0b9
Kendrick ACER <kendrickyanuar@gmail.com>
2025-10-13 11:53:18 +1100

    Done 2015, day 9, part 1
    Properly this time. Also, its NOT a TSP, but its variant Shortest Hamiltonian path.

--- 2015/day_09/part_1.py
+++ 2015/day_09/part_1.py
@@ -1,4 +1,5 @@
 # Travelling Salesman Problem, except Santa does not return back to starting city.
+# Also known as Shortest Hamiltonian path variant.
 
 import os
 import logging, logging.handlers
@@ -43,26 +44,28 @@ def write_logs(filename: str = 'general.log', log_level: int = logging.DEBUG) ->
 def get_input(filename: str = 'input.txt') -> str:
     with open(filename, 'r') as f:
         return f.read()
-    
+
 class FindShortestRoute:
     '''
     Takes a list of distances.
-    
+
     Returns the distance of the shortest route between any 2 locations.
     '''
 
     def __init__(self, distances_list: list[str]) -> None:
         self.distances_data = self.parse_data(distances_list)
         self.total_num_cities = len(self.distances_data.keys())
-        logging.debug(f'self.distances_data:\n{self.distances_data}\ntotal: {self.total_num_cities}')
-        self.path = []
-        self.cost = 0
+        logging.debug(f'self.distances_data:\n{self.distances_data}')
+        self.all_paths = []
+        self.current_path = []
         self.next_city = ''
+        self.cost = 0
+        self.path_taken = [] 
         self.result = 0
 
-        self.run()
+        self.path_taken, self.result = self.run()
 
-    def parse_data(self, data: list[str]) -> dict[str, dict[str, int]]:
+    def parse_data(self, data: list[str]) -> dict[str, list[tuple[str, int]]]:
         parsed_data = {}
         for line in data:
             sep_words = line.split(' ')
@@ -72,44 +75,49 @@ class FindShortestRoute:
             key_list = parsed_data.keys()
 
             if city_1 in key_list:
-                parsed_data[city_1][city_2] = dist
+                parsed_data[city_1].append((city_2, dist))
             else:
-                parsed_data[city_1] = {city_2 : dist}
-                
-            if city_2 in key_list:
-                parsed_data[city_2][city_1] = dist
-            else:
-                parsed_data[city_2] = {city_1 : dist}
+                parsed_data[city_1] = [(city_2, dist)]
 
+            if city_2 in key_list:
+                parsed_data[city_2].append((city_1, dist))
+            else:
+                parsed_data[city_2] = [(city_1, dist)]
+
+        parsed_data = {k : sorted(v, key=lambda x: x[1]) for k, v in parsed_data.items()}
         return parsed_data
-    
-    def visit_next_city(self, current_city: str) -> int | None:
-        next_closest_city = min(self.distances_data[current_city], key=self.distances_data[current_city].get)
-        logging.debug(f'current_city: {current_city}, next_closest_city: {next_closest_city}')
-        if next_closest_city in self.path:
-            logging.info(f'Reached dead end :(\
-                         \npath: {self.path}\
-                         \nnext_closest_city: {next_closest_city}\
-                         \nresult: {self.result}')
-            return -1
-        self.result += self.distances_data[current_city][next_closest_city]
-        self.path.append(next_closest_city)
-        self.visit_next_city(next_closest_city)
-    
-    def run(self) -> int | None:
-        starting_city = 'AlphaCentauri'
-        self.path.append(starting_city)
-        ret = self.visit_next_city(starting_city)
-        print(ret)
-        if ret < 0:
-            print('Failed!')
+
+    def greedy_path_from(self, current_city: str) -> int | None:
+        next_closest_city = ''
+        for value in self.distances_data[current_city]:
+            if value[0] in self.current_path:
+                continue
+            next_closest_city = value[0]
+            self.cost += value[1]
+            self.current_path.append(next_closest_city)
+            return self.greedy_path_from(next_closest_city)
+        else:
+            return 0
+
+    def run(self) -> tuple[list[str], int]:
+        for starting_city in self.distances_data.keys():
+            self.current_path.append(starting_city)
+            ret = self.greedy_path_from(starting_city)
+            if ret is not None and ret == 0:
+                logging.debug(f'Completed path: {self.current_path}, Cost: {self.cost}')
+                self.all_paths.append((self.current_path, self.cost))
+                self.current_path = []
+                self.cost = 0
+                continue
+        sorted_paths = sorted(self.all_paths, key=lambda x: x[1])
+        return sorted_paths[0]
 
 def main() -> None:
     write_logs('part_1.log')
     distances_list = get_input().splitlines()
     shortest_route = FindShortestRoute(distances_list)
     print(f'Answer: {shortest_route.result}')
-    print(f'Path taken: {shortest_route.path}')
+    print(f'Path taken: {shortest_route.path_taken}')
 
 if __name__ == '__main__':
     main()
