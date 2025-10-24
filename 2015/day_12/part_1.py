6782e157aa4226a1743a627ef7b484e55699e126
Kendrick ACER <kendrickyanuar@gmail.com>
2025-10-24 16:19:23 +1100

    Started 2015, day 12, part 1

---
+++ 2015/day_12/part_1.py
@@ -0,0 +1,57 @@
+import os
+import logging, logging.handlers
+import json
+
+def write_logs(filename: str = 'general.log', log_level: int = logging.DEBUG) -> None:
+    """
+    Create a logs folder if it does not already exist. Write simple logs into a file in the logs folder.
+
+    Parameter
+    ---------
+    filename : str, default: general.log
+        The name of the log file, with .log extension. By default, the log file will be written in ./logs/general.log.
+
+    log_level : int, default: logging.debug
+        The lowest level of logs that will be saved. By default, log at debug level.
+
+    Returns
+    -------
+    None
+
+    Other Parameters
+    ----------------
+    imported_module : os
+        Used to check if logs folder exists and creates it if it does not.
+
+    imported_module : logging
+        Used to write and configure the log file.
+
+    imported_module : logging.handlers
+        Used to handle rotation of log files. logging.handlers.RotatingFileHandler is set to 10MB, 5 files.
+
+    Notes
+    -----
+    Just using a simple basicConfig to write the logs. Planning to use more personalised logs in the future.
+    """
+
+    if not os.path.exists('logs'):
+        os.makedirs('logs', exist_ok=True)
+    logging.basicConfig(filename='logs/' + filename, filemode='w', format='[%(asctime)s] %(levelname)s | %(name)s - %(message)s', level=log_level, datefmt='%Y-%m-%d %H:%M:%S')
+    logging.handlers.RotatingFileHandler(filename='logs/' + filename, maxBytes=10*1024*1024, backupCount=5) # 10MB, 5 files max
+
+def get_input(filename: str) -> str:
+    with open(filename, 'r') as f:
+        return json.load(f)
+
+class SomeClass:
+
+    def __init__(self) -> None:
+        pass
+
+    def run(self) -> None:
+        pass
+
+if __name__ == '__main__':
+    write_logs('part_1.log')
+    puzzle_input = get_input('input.json')
+    print(puzzle_input)
